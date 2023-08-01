1faf3e9b0d15cdb3c7be7c0e416e245ae8851af3
krzysiekk22d <klas.club@gmail.com>
2023-08-01 06:45:21 +0200

    Add a label. Add two constants.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let number = 0
+        let word = "abcd"
         // Do any additional setup after loading the view.
     }
 
